08369d4e7d7362b74cf2ea48edb9d11bb83e0236
navoren <namantomar1453@gmail.com>
2024-01-09 23:29:11 +0530

    final commit

--- App.tsx
+++ App.tsx
@@ -1,7 +1,10 @@
 /* eslint-disable prettier/prettier */
+import { Formik } from 'formik';
 import React, { useState } from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import BouncyCheckbox from 'react-native-bouncy-checkbox';
 import * as Yup from 'yup';
+
 
 const PasswordSchema = Yup.object().shape({
   passwordLength: Yup.number()
@@ -67,19 +70,206 @@ export default function App() {
   };
 
   return (
-    <SafeAreaView>
-    <View>
-      <Text style={styles.headingText}>Password Generator</Text>
-      </View>
+    <ScrollView keyboardShouldPersistTaps="handled">
+      <SafeAreaView style={styles.appContainer}>
+        <View style={styles.formContainer}>
+          <Text style={styles.title}>Password Generator</Text>
+            <Formik
+        initialValues={{ passwordLength : '' }}
+            validationSchema={PasswordSchema}
+            onSubmit={values => {
+              generatePasswordString(+values.passwordLength);
+            }}
+        >
+        {({
+          values,
+          errors,
+          touched,
+          isValid,
+          handleChange,
+          handleReset,
+          handleSubmit,
+          isSubmitting,
+          /* and other goodies */
+        }) => (
+              <>
+                <View style={styles.inputWrapper}>
+                  <View style={styles.inputColumn}>
+                    <Text style={styles.heading}> Password Length</Text>
+                    {touched.passwordLength && errors.passwordLength && (
+                      <Text style={styles.errorText}>
+                        {errors.passwordLength}
+                      </Text>
+                    )}
+                  </View>
+                  <TextInput
+                      style={styles.inputStyle}
+                      value={values.passwordLength}
+                      onChangeText={handleChange('passwordLength')}
+                      placeholder='Example text'
+                      keyboardType='numeric'
+                    />
+                </View>
+                <View style={styles.inputWrapper}>
+                  <Text style={styles.heading}> Include Lowercase</Text>
+                  <BouncyCheckbox
+                    disableBuiltInState
+                    isChecked={lowerCase}
+                  onPress={()=> setLowerCase(!lowerCase)}/>
+                </View>
+                <View style={styles.inputWrapper}>
+                <Text style={styles.heading}> Include Uppercase</Text>
+                  <BouncyCheckbox
+                    disableBuiltInState
+                    isChecked={upperCase}
+                  onPress={()=> setUpperCase(!upperCase)}/>
+                </View>
+                <View style={styles.inputWrapper}>
+                <Text style={styles.heading}> Include Digits</Text>
+                <BouncyCheckbox
+                    disableBuiltInState
+                    isChecked={number}
+                  onPress={()=> setNumber(!number)}/>
+                </View>
+                <View style={styles.inputWrapper}>
+                <Text style={styles.heading}> Include Symbols</Text>
+                <BouncyCheckbox
+                    disableBuiltInState
+                    isChecked={symbols}
+                  onPress={()=> setSymbols(!symbols)}/>
+                </View>
+                <View style={styles.formActions}>
+                  <TouchableOpacity
+                    disabled={!isValid}
+                    style={styles.primaryBtn}
+                    onPress={handleSubmit}
+                  >
+                    <Text style={styles.primaryBtnTxt}>Generate Password</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    disabled={!isValid}
+                    style={styles.secondaryBtn}
+                    onPress={() => {
+                      handleReset();
+                      resetPasswordState();
+                  }}>
+                    <Text style={styles.secondaryBtnTxt}>Reset</Text>
+                  </TouchableOpacity>
+                </View>
+              </>
+        )}
+      </Formik>
+        </View>
+        {isPassGen ? (
+          <View style={[styles.card, styles.cardElevated]}>
+            <Text style={styles.subTitle}> Result </Text>
+            <Text style={styles.description}>Long Press to Copy</Text>
+            <Text selectable={true} style={styles.generatedPassword}>
+              {pass}
+            </Text>
+            </View>
+        ):null}
+
       </SafeAreaView>
+    </ScrollView>
+    
   )
 }
 
 const styles = StyleSheet.create({
-  headingText: {
-    fontSize: 28,
+  appContainer: {
+    flex: 1,
+  },
+  formContainer: {
+    margin: 8,
+    padding: 8,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: '600',
+    marginBottom: 15,
+  },
+  subTitle: {
+    fontSize: 26,
+    fontWeight: '600',
+    marginBottom: 2,
+  },
+  description: {
+    color: '#758283',
+    marginBottom: 8,
+  },
+  heading: {
+    fontSize: 15,
+  },
+  inputWrapper: {
+    marginBottom: 15,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+  },
+  inputColumn: {
+    flexDirection: 'column',
+  },
+  inputStyle: {
+    padding: 8,
+    width: '30%',
+    borderWidth: 1,
+    borderRadius: 4,
+    borderColor: '#16213e',
+  },
+  errorText: {
+    fontSize: 12,
+    color: '#ff0d10',
+  },
+  formActions: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  primaryBtn: {
+    width: 120,
+    padding: 10,
+    borderRadius: 8,
+    marginHorizontal: 8,
+    backgroundColor: '#5DA3FA',
+  },
+  primaryBtnTxt: {
+    color: '#fff',
+    textAlign: 'center',
+    fontWeight: '700',
+  },
+  secondaryBtn: {
+    width: 120,
+    padding: 10,
+    borderRadius: 8,
+    marginHorizontal: 8,
+    backgroundColor: '#80BCBD',
+  },
+  secondaryBtnTxt: {
+    textAlign: 'center',
+    marginTop: 8,
     fontWeight: 'bold',
-    padding : 8,
-
+  },
+  card: {
+    padding: 12,
+    borderRadius: 6,
+    marginHorizontal: 12,
+  },
+  cardElevated: {
+    backgroundColor: '#2b3a63',
+    elevation: 1,
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+    shadowColor: '#333',
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+  },
+  generatedPassword: {
+    fontSize: 22,
+    textAlign: 'center',
+    marginBottom: 12,
+    color: '#fff',
+    fontWeight: 'bold',
   },
 });
